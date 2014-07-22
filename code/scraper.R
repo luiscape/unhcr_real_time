55cd7a23f43f013f4294cedc18b259756a4b0bba
Luis Capelo <luiscape@gmail.com>
2014-07-22 16:45:33 -0400

    adding dsID and updating indID

--- code/scraper.R
+++ code/scraper.R
@@ -4,6 +4,7 @@
 # Use this data with caution.
 
 library(rjson)
+library(RCurl)
 
 # ScraperWiki's base URL
 sw_url <- 'https://ds-ec2.scraperwiki.com/ye7nhjd/uq816xboyfzffx6/sql?q=select * from unhcr_real_time'
@@ -18,8 +19,8 @@ for (i in 1:length(fromJSON(doc))) {
 
 # indicator
 ind_list <- unique(data$module_name)
-indicator <- data.frame(indID = c('CHD.B.PRO.02.T2','CHD.B.PRO.03.T2','CHD.B.PRO.04.T2','CHD.B.PRO.05.T2','CHD.B.PRO.06.T2','CHD.B.PRO.07.T2','CHD.B.PRO.08.T2','CHD.B.PRO.09.T2',
-'CHD.B.PRO.10.T2','CHD.B.PRO.11.T2','CHD.B.PRO.12.T2','CHD.B.PRO.13.T2','CHD.B.PRO.14.T2'), name = ind_list, units = 'People')
+indicator <- data.frame(indID = c('CHD.O.PRO.20.T2','CHD.O.PRO.21.T2','CHD.O.PRO.22.T2','CHD.O.PRO.23.T2','CHD.O.PRO.24.T2','CHD.O.PRO.25.T2','CHD.O.PRO.26.T2','CHD.O.PRO.27.T2',
+'CHD.O.PRO.28.T2','CHD.O.PRO.29.T2','CHD.O.PRO.30.T2','CHD.O.PRO.31.T2','CHD.O.PRO.32.T2'), name = ind_list, units = 'People')
 
 # dataset
 dataset <- data.frame(dsID = 'unhcr-real-time', 
@@ -39,7 +40,8 @@ data$is_number <- 1
 data$region <- ifelse(data$region == 'SYRIA', 'SYR', data$region)
 data$region <- ifelse(data$region == 'SOUTHSUDAN', 'SSD', data$region)
 data$region <- ifelse(data$region == 'LIBERIA', 'LBR', data$region)
-data$region <- ifelse(data$region == 'HORN', 'SOM', data$region) 
+data$region <- ifelse(data$region == 'HORN', 'SOM', data$region)
+data$dsID <- 'unhcr-real-time'
 
 value <- data
 
